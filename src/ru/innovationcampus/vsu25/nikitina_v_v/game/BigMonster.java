a0a8d86e9e83af4b5f817aa454ea2d0ae1f026d6
Вероника <nikver@gmail.com>
2025-10-27 13:00:19 +0300

    finish

--- src/ru/innovationcampus/vsu25/nikitina_v_v/game/BigMonster.java
+++ src/ru/innovationcampus/vsu25/nikitina_v_v/game/BigMonster.java
@@ -2,46 +2,71 @@ package ru.innovationcampus.vsu25.nikitina_v_v.game;
 
 import java.util.Scanner;
 
-public class BigMonster extends Monster{
-    private String image = "\uD83D\uDC79";
+public class BigMonster extends Monster {
+    private final String image = "\uD83D\uDC79";
+    private final Scanner sc = new Scanner(System.in);
+
     public String getImage() {
         return image;
     }
+
     BigMonster(int sizeBoard) {
         super(sizeBoard);
     }
-    @Override
-    public boolean taskMonster(int difficultGame){
-        if (difficultGame == 1) {
-            return taskMonster();
-        } else {
-            int x = r.nextInt(25 * difficultGame);
-            int y = r.nextInt(25);
-            int z = r.nextInt(100);
-            int q = r.nextInt(25 * difficultGame);
-            int w = r.nextInt(25);
-            int trueAnswer2 = x * y - z;
-            int trueAnswer1 = q * w;
-            System.out.println("О нет! Походу у монстра есть щит!");
-            System.out.println("Чтобы его сломать, найдите площадь щита. Первая сторона равна " + q + ", а вторая равна " + w);
-            Scanner sc = new Scanner(System.in);
-            int Answer1 = sc.nextInt();
-            if (trueAnswer1 == Answer1) {
-                System.out.println("Молодец, а теперь разберись с монстром");
-                System.out.println("Реши пример: " + x + " * " + y + " + " + z + " = ?");
 
-                int Answer2 = sc.nextInt();
-                if (trueAnswer2 == Answer2) {
-                    System.out.println("Верно! Ты победил монстра");
-                    return true;
-                }
-                System.out.println("Ты проиграл эту битву!");
-                return false;
-            }else {System.out.println("Ты проиграл эту битву!");
-                return false;}
+    @Override
+    public boolean taskMonster(int difficultGame) {
+        if (difficultGame == 1) {
+            return simpleTaskMonster();
+        } else {
+            return complexTaskMonster(difficultGame);
         }
     }
-    public boolean taskMonster() {
+
+    private boolean complexTaskMonster(int difficultGame) {
+        int x = r.nextInt(25 * difficultGame);
+        int y = r.nextInt(25);
+        int z = r.nextInt(100);
+        int q = r.nextInt(2 * difficultGame);
+        int w = r.nextInt(6);
+        int trueAnswer2 = x * y + z;
+        int trueAnswer1 = q * w;
+        int strong = r.nextInt(3);
+
+        if (strong == 1 || strong == 2) {
+            System.out.println("Реши пример: " + x + " * " + y + " + " + z + " = ?");
+            int Answer2 = sc.nextInt();
+
+            if (trueAnswer2 == Answer2) {
+                System.out.println("Верно! Ты победил монстра");
+                return true;
+            }
+            System.out.println("Ты проиграл эту битву!");
+            return false;
+        } else {
+            System.out.println("О нет! Похоже у монстра есть щит!");
+            System.out.println("Чтобы его сломать, найдите площадь щита. Первая сторона равна " + q + ", а вторая равна " + w);
+
+            int Answer1 = sc.nextInt();
+            if (trueAnswer1 != Answer1) {
+                System.out.println("Ты проиграл эту битву!");
+                return false;
+            }
+
+            System.out.println("Молодец, а теперь разберись с монстром");
+            System.out.println("Реши пример: " + x + " * " + y + " + " + z + " = ?");
+
+            int Answer2 = sc.nextInt();
+            if (trueAnswer2 == Answer2) {
+                System.out.println("Верно! Ты победил монстра");
+                return true;
+            }
+            System.out.println("Ты проиграл эту битву!");
+            return false;
+        }
+    }
+
+    public boolean simpleTaskMonster() {
         return super.taskMonster(0);
     }
 }
--- src/ru/innovationcampus/vsu25/nikitina_v_v/game/Monster.java
+++ src/ru/innovationcampus/vsu25/nikitina_v_v/game/Monster.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Monster {
     private String image = "\uD83E\uDDDF\u200D";
-    private int x,y;
+    private int x, y;
     Random r = new Random();
 
     public String getImage() {
@@ -25,22 +25,52 @@ public class Monster {
         this.x = r.nextInt(sizeBoard - 1);
         this.y = r.nextInt(sizeBoard);
     }
+
     //если мне расскажут как сделать вероятность, я сделаю так чтобы при небольшом шансе у монстра был тотем возрождения. Типо его надо победить 2 раза
-    public boolean conflictPerson(int perX, int perY){
-        return perY - 1 == this.y && perX -1 == this.x;
+    public boolean conflictPerson(int perX, int perY) {
+        return perY - 1 == this.y && perX - 1 == this.x;
     }
+
     public boolean taskMonster(int difficultGame) {
         int x = r.nextInt(300);
         int y = r.nextInt(100 * difficultGame);
-        int trueAnswer = x + y;
-        System.out.println("Реши пример: " + x + " + " + y + " = ?");
+        int trueAnswer2 = x + y;
+        int strong = r.nextInt(3);
+        int q = r.nextInt(25 * difficultGame);
+        int w = r.nextInt(25);
+        int trueAnswer1 = q + w;
         Scanner sc = new Scanner(System.in);
-        int Answer = sc.nextInt();
-        if (trueAnswer == Answer) {
-            System.out.println("Верно! Ты победил монстра");
-            return true;
+        if (strong == 1 || strong == 2) {
+            System.out.println("Реши пример: " + x + " + " + y +" = ?");
+            int Answer2 = sc.nextInt();
+
+            if (trueAnswer2 == Answer2) {
+                System.out.println("Верно! Ты победил монстра");
+                return true;
+            }
+            System.out.println("Ты проиграл эту битву!");
+            return false;
+        } else {
+            System.out.println("О нет! Похоже у монстра 2 жизни!");
+            System.out.println("Реши пример: " + x + " + " + y +" = ?");
+
+            int Answer2 = sc.nextInt();
+            if (trueAnswer2 != Answer2) {
+                System.out.println("Ты проиграл эту битву!");
+                return false;
+            }
+
+            System.out.println("Молодец, а теперь победи его окончательно!");
+            System.out.println("Реши пример: " + q + " + " + w +" = ?");
+
+            int Answer1 = sc.nextInt();
+            if (trueAnswer1 == Answer1) {
+                System.out.println("Верно! Ты победил монстра");
+                return true;
+            }
+            System.out.println("Ты проиграл эту битву!");
+            return false;
         }
-        System.out.println("Ты проиграл эту битву!");
-        return false;
+
+        }
     }
-}
